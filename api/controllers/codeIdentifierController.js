c4665519b8c0bd183af3f9fb5382fc52a9f6d1db
filia alexis <filia.alexis@gmail.com>
2020-12-09 18:48:15 +0100

    Add procfile6

--- api/controllers/codeIdentifierController.js
+++ api/controllers/codeIdentifierController.js
@@ -3,7 +3,7 @@ const hljs = require('../../vendors/highlightjs/highlight.pack.js');
 exports.identify_code = function(req, res) {
   const test = hljs.highlightAuto(req.body.code);
   let result={"language1":`${test.top.name}`,"relevance1":`${test.top.relevance}`,"language2":`${test.second_best.language}`,"relevance2":`${test.second_best.relevance}`};
-  console.log(result);
+  //console.log(result);
   res.end(JSON.stringify(result));
 };
 
--- api/routes/codeIdentifierRoutes.js
+++ api/routes/codeIdentifierRoutes.js
@@ -2,11 +2,6 @@
 module.exports = function(app) {
   var codeIdentifier = require('../controllers/codeIdentifierController');
 
-  // codeIdentifier Routes
-
-  // app.route('/codes_identified')
-  //   .get(codeIdentifier.codes_identified);
-
   app.route('/identify_code')
     .post(codeIdentifier.identify_code);
 };
